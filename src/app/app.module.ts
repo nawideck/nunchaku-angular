1eba38501fffded220551b5c05984a2c0ad3ae99
Anthony Batifol <nawideck@gmail.com>
2021-03-07 21:19:20 +0100

    feat(header-footer): initialize header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExamplesModule } from './features/examples/examples.module';
+import { FooterModule } from './shared/components/footer/footer.module';
+import { NavbarModule } from './shared/components/navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ExamplesModule],
+  imports: [BrowserModule, AppRoutingModule, ExamplesModule, NavbarModule, FooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
